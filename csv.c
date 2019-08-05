2f8bae6eafb06bb5f6c66ca69aaefd1b2d8ed5b1
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2019-08-05 15:36:37 -0400

    Adicionar arquivo ao projeto
    Adicionar arquivo 'csv.c' ao projeto

---
+++ csv.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+double** readCsv(const char *filename) {
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+
+    fclose(fp);
+    return NULL;
+}
+
+int main() {
+
+    return 0;
+}
